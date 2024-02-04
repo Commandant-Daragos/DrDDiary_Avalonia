b338998742b83b80e4bbd9cb304cac05c2a1ab4f
Dominik Medvecký <dominikmedvecky@gmail.com>
2024-02-04 14:55:46 +0100

    Add project files.

---
+++ DrDDiary/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DrDDiary.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DrDDiary/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DrDDiary.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
