43dacf37f177d516d8f205507984bf082a924197
Dominik Medvecký <dominikmedvecky@gmail.com>
2024-05-05 21:19:45 +0200

    Create basic labels for all available classes, add comment to distinguish code parts

--- DrDDiary/Views/CharacterView.axaml.cs
+++ DrDDiary/Views/CharacterView.axaml.cs
@@ -17,6 +17,25 @@ namespace DrDDiary.Views
             //Solo method invoked by Event called when button to change language is called ?
             nameOfCharacter.Content = LanguageManager.GetString("lblName");
             raceOfCharacter.Content = LanguageManager.GetString("lblRace");
+            //Classes-Basic
+            ClassesBasic.Content = LanguageManager.GetString("lblClassesBasic");
+            lblFighter.Content = LanguageManager.GetString("lblFighter");
+            lblTrickster.Content = LanguageManager.GetString("lblTrickster");
+            lblHunter.Content = LanguageManager.GetString("lblHunter");
+            lblMedicaster.Content = LanguageManager.GetString("lblMedicaster");
+            lblConjurer.Content = LanguageManager.GetString("lblConjurer");
+            //Classes-Advanced
+            ClassesAdvanced.Content = LanguageManager.GetString("lblClassesAdvanced");
+            lblWarrior.Content = LanguageManager.GetString("lblWarrior");
+            lblBorderGuard.Content = LanguageManager.GetString("lblBorderGuard");
+            lblScholar.Content = LanguageManager.GetString("lblScholar");
+            lblWizard.Content = LanguageManager.GetString("lblWizard");
+            lblScout.Content = LanguageManager.GetString("lblScout");
+            lblRobber.Content = LanguageManager.GetString("lblRobber");
+            lblMage.Content = LanguageManager.GetString("lblMage");
+            lblDruid.Content = LanguageManager.GetString("lblDruid");
+            lblShaman.Content = LanguageManager.GetString("lblShaman");
+            lblAlchemist.Content = LanguageManager.GetString("lblAlchemist");
         }
     }
 }
